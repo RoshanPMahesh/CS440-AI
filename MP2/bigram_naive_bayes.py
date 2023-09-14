6ff5c04935133e5057bc7a5459c1e83b97dfa90a
Roshan Mahesh <roshanprakashmahesh99@gmail.com>
2023-09-14 01:22:34 -0500

    Debugging Needed

--- MP2/bigram_naive_bayes.py
+++ MP2/bigram_naive_bayes.py
@@ -20,6 +20,7 @@ so be careful to not modify anything else.
 import reader
 import math
 from tqdm import tqdm
+import numpy as npy
 from collections import Counter
 
 
@@ -53,14 +54,144 @@ Main function for training and predicting with the bigram mixture model.
     You can modify the default values for the Laplace smoothing parameters, model-mixture lambda parameter, and the prior for the positive label.
     Notice that we may pass in specific values for these parameters during our testing.
 """
-def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=1.0, bigram_laplace=1.0, bigram_lambda=1.0, pos_prior=0.5, silently=False):
-    print_values_bigram(unigram_laplace,bigram_laplace,bigram_lambda,pos_prior)
+def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=1.0, bigram_laplace=0.1, bigram_lambda=0.1, pos_prior=0.75, silently=False):
+    print_values_bigram(unigram_laplace, bigram_laplace, bigram_lambda, pos_prior)
 
+    # unigram part of code
     yhats = []
-    for doc in tqdm(dev_set, disable=silently):
-        yhats.append(-1)
+    uni_pos = []
+    uni_neg = []
+    bi_pos = []
+    bi_neg = []
+    neg_prior = 1 - pos_prior
+
+    # trains the naive bayes algorithm by going through the training set data
+    negative_known_prob, negative_unknown_prob = trainingPhase(train_set, train_labels, unigram_laplace, 0)
+    positive_known_prob, positive_unknown_prob = trainingPhase(train_set, train_labels, unigram_laplace, 1)
+    
+    # goes through the development set to calculate whether the reviews are positive or negative
+    for individual_set in dev_set:
+        positive_words_prob = 0
+        negative_words_prob = 0
+
+        positive_words_prob += npy.log(pos_prior)
+        negative_words_prob += npy.log(neg_prior)
+
+        # calculates the probability of each review being positive or negative
+        # takes the log function to prevent underflow
+        for words in individual_set:
+            if words in negative_known_prob:
+                negative_words_prob += npy.log(negative_known_prob[words])
+            else:
+                negative_words_prob += npy.log(negative_unknown_prob)
+            
+            if words in positive_known_prob:
+                positive_words_prob += npy.log(positive_known_prob[words])
+            else:
+                positive_words_prob += npy.log(positive_unknown_prob)
+
+        uni_pos.append(positive_words_prob)
+        uni_neg.append(negative_words_prob)
+        
+    # bigram part of code
+    bigram_negative_known_prob, bigram_negative_unknown_prob = bigramTrainingPhase(train_set, train_labels, bigram_laplace, 0)
+    bigram_positive_known_prob, bigram_positive_unknown_prob = bigramTrainingPhase(train_set, train_labels, bigram_laplace, 1)
+
+    for b_individual_set in dev_set:
+        b_positive_words_prob = 0
+        b_negative_words_prob = 0
+
+        b_positive_words_prob += npy.log(pos_prior)
+        b_negative_words_prob += npy.log(neg_prior)
+
+        for createTuples in range(len(b_individual_set) - 1):
+            dev_set_tuple = tuple((b_individual_set[createTuples], b_individual_set[createTuples + 1]))
+
+            if dev_set_tuple in bigram_negative_known_prob:
+                b_negative_words_prob += npy.log(bigram_negative_known_prob[dev_set_tuple])
+            else:
+                b_negative_words_prob += npy.log(bigram_negative_unknown_prob)
+
+            if dev_set_tuple in bigram_positive_known_prob:
+                b_positive_words_prob += npy.log(bigram_positive_known_prob[dev_set_tuple])
+            else:
+                b_positive_words_prob += npy.log(bigram_positive_unknown_prob)
+
+        bi_pos.append(b_positive_words_prob)
+        bi_neg.append(b_negative_words_prob)
+
+    for mixture_model in range(len(dev_set)):
+        positives = ((1 - bigram_lambda) * uni_pos[mixture_model]) + ((bigram_lambda) * bi_pos[mixture_model])
+        negatives = ((1 - bigram_lambda) * uni_neg[mixture_model]) + ((bigram_lambda) * bi_neg[mixture_model])
+
+        if positives >= negatives:
+            yhats.append(1)
+        else:
+            yhats.append(0)
 
     return yhats
+                
+
+def trainingPhase(train_set, train_labels, laplace_smoothing, type):
+    review_num = len(train_labels)
+    words = {}
+    known_prob = {}
+    unknown_prob = 0
+    total_words = 0
+    total_types = 0
+
+    # calculates the number of words for each word in a positive or negative review - stored in list "words"
+    # example -> like: 15, hate: 17, sad: 3
+    for i in range(review_num):
+        if (train_labels[i] == type):
+            for train_word in train_set[i]:
+                if train_word in words:
+                    words[train_word] += 1
+                else:
+                    words[train_word] = 1
+
+    # calculates the probability of a word while applying laplace smoothing equation
+    # doing this for unknown probability, which are all the unseen words, and for the known probability, which are all the seen words
+    total_types = len(words)
+    for word_counter in words:
+        total_words += words[word_counter]
+
+    for word_counter_new in words:
+        known_prob[word_counter_new] = (words[word_counter_new] + laplace_smoothing) / (total_words + (laplace_smoothing * (total_types + 1)))
+
+    unknown_prob = laplace_smoothing / (total_words + (laplace_smoothing * (total_types + 1)))
+
+    return known_prob, unknown_prob
+
+
+def bigramTrainingPhase(train_set, train_labels, laplace_smoothing, type):
+    bigramWords = {}
+    review_num = len(train_labels)
+    known_prob = {}
+    unknown_prob = 0
+    total_words = 0
+    total_types = 0
+
+    for i in range(review_num):
+        if train_labels == type:
+            for j in range(len(train_set[i]) - 1):
+                eachTuple = tuple(train_set[i][j], train_set[i][j + 1])
+                if eachTuple in bigramWords:
+                    bigramWords[eachTuple] += 1
+                else:
+                    bigramWords[eachTuple] = 1
+
+    total_types = len(bigramWords)
+    for word_counter in bigramWords:
+        total_words += bigramWords[word_counter]
+
+    for word_counter_new in bigramWords:
+        known_prob[word_counter_new] = (bigramWords[word_counter_new] + laplace_smoothing) / (total_words + (laplace_smoothing * (total_types + 1)))
+
+    unknown_prob = laplace_smoothing / (total_words + (laplace_smoothing * (total_types + 1)))
+
+    return known_prob, unknown_prob
+    
 
 
 
