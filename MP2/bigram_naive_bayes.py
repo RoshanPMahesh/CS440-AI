d3ce58f71dde32e6d2a0fffbc09aca68c552afc0
Roshan Mahesh <roshanprakashmahesh99@gmail.com>
2023-09-15 01:42:20 -0500

    89.2% Accuracy

--- MP2/bigram_naive_bayes.py
+++ MP2/bigram_naive_bayes.py
@@ -54,15 +54,37 @@ Main function for training and predicting with the bigram mixture model.
     You can modify the default values for the Laplace smoothing parameters, model-mixture lambda parameter, and the prior for the positive label.
     Notice that we may pass in specific values for these parameters during our testing.
 """
-def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=0.15, bigram_laplace=0.1, bigram_lambda=0.9, pos_prior=0.75, silently=False):
-    print_values_bigram(unigram_laplace, bigram_laplace, bigram_lambda, pos_prior)
-
-    # unigram part of code
+def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=0.009, bigram_laplace=0.007, bigram_lambda=0.42, pos_prior=0.8, silently=False):
     yhats = []
+    
+    # checking for correct input of parameters
+    print_values_bigram(unigram_laplace, bigram_laplace, bigram_lambda, pos_prior)
+    
+    # does calculation of P(Y) + P(w0|Y) + P(w1|Y) +...+ P(wn|Y) for unigram and bigram
+    uni_pos, uni_neg = unigramPart(dev_set, train_set, train_labels, unigram_laplace, pos_prior)
+    bi_pos, bi_neg = bigramPart(dev_set, train_set, train_labels, bigram_laplace, pos_prior)
+
+    # calculates the mixture model which takes the unigram and bigram to determine if a review is positive or negative
+    for mixture_model in range(len(dev_set)):
+        positives = mixtureModelMath(mixture_model, bigram_lambda, uni_pos, bi_pos)
+        negatives = mixtureModelMath(mixture_model, bigram_lambda, uni_neg, bi_neg)
+
+        if positives >= negatives:
+            yhats.append(1)
+        else:
+            yhats.append(0)
+
+    return yhats
+
+# does the math for the mixture model
+def mixtureModelMath(mixture_model, bigram_lambda, unigram_prob, bigram_prob):
+    result = ((1 - bigram_lambda) * unigram_prob[mixture_model]) + ((bigram_lambda) * bigram_prob[mixture_model])
+    return result
+
+# does calculation of P(Y) + P(w0|Y) + P(w1|Y) +...+ P(wn|Y) for unigram
+def unigramPart(dev_set, train_set, train_labels, unigram_laplace, pos_prior):
     uni_pos = []
     uni_neg = []
-    bi_pos = []
-    bi_neg = []
     neg_prior = 1 - pos_prior
 
     # trains the naive bayes algorithm by going through the training set data
@@ -90,13 +112,23 @@ def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=0.15, bigram_l
             else:
                 positive_words_prob += npy.log(positive_unknown_prob)
 
+        # appends the probability of a review being positive or negative into a list
         uni_pos.append(positive_words_prob)
         uni_neg.append(negative_words_prob)
-        
-    # bigram part of code
+
+    return uni_pos, uni_neg
+
+# does calculation of P(Y) + P(w0|Y) + P(w1|Y) +...+ P(wn|Y) for bigram
+def bigramPart(dev_set, train_set, train_labels, bigram_laplace, pos_prior):
+    bi_pos = []
+    bi_neg = []
+    neg_prior = 1 - pos_prior
+
+    # trains the bigram naive bayes algorithm by going through the training set data
     bigram_negative_known_prob, bigram_negative_unknown_prob = bigramTrainingPhase(train_set, train_labels, bigram_laplace, 0)
     bigram_positive_known_prob, bigram_positive_unknown_prob = bigramTrainingPhase(train_set, train_labels, bigram_laplace, 1)
 
+    # goes through the development set to calculate whether the reviews are positive or negative
     for b_individual_set in dev_set:
         b_positive_words_prob = 0
         b_negative_words_prob = 0
@@ -104,9 +136,16 @@ def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=0.15, bigram_l
         b_positive_words_prob += npy.log(pos_prior)
         b_negative_words_prob += npy.log(neg_prior)
 
+        # iterates through each word in review
+        # takes the log function to account for underflow
         for createTuples in range(len(b_individual_set) - 1):
+
+            # creates a tuple by taking the current word and the next word of the review
+            # this then creates the bigram
             dev_set_tuple = tuple((b_individual_set[createTuples], b_individual_set[createTuples + 1]))
 
+            # calculates the probability of each bigram being positive or negative and adds them with previous tuples
+            # this then calculates the probability for the entire review by the time createTuples reaches the end
             if dev_set_tuple in bigram_negative_known_prob:
                 b_negative_words_prob += npy.log(bigram_negative_known_prob[dev_set_tuple])
             else:
@@ -117,20 +156,12 @@ def bigramBayes(dev_set, train_set, train_labels, unigram_laplace=0.15, bigram_l
             else:
                 b_positive_words_prob += npy.log(bigram_positive_unknown_prob)
 
+        # appends the probability of a review being positive or negative into a list
         bi_pos.append(b_positive_words_prob)
         bi_neg.append(b_negative_words_prob)
 
-    for mixture_model in range(len(dev_set)):
-        positives = ((1 - bigram_lambda) * uni_pos[mixture_model]) + ((bigram_lambda) * bi_pos[mixture_model])
-        negatives = ((1 - bigram_lambda) * uni_neg[mixture_model]) + ((bigram_lambda) * bi_neg[mixture_model])
+    return bi_pos, bi_neg
 
-        if positives >= negatives:
-            yhats.append(1)
-        else:
-            yhats.append(0)
-
-    return yhats
-                
 
 def trainingPhase(train_set, train_labels, laplace_smoothing, type):
     review_num = len(train_labels)
@@ -172,15 +203,22 @@ def bigramTrainingPhase(train_set, train_labels, laplace_smoothing, type):
     total_words = 0
     total_types = 0
 
+    # calculates the number of tuples for each tuple in a positive or negative review - stored in list "bigramWords"
+    # example -> like, movie: 15, hate, seats: 17, sad, price: 3
     for i in range(review_num):
-        if train_labels == type:
+        if train_labels[i] == type:
             for j in range(len(train_set[i]) - 1):
-                eachTuple = tuple(train_set[i][j], train_set[i][j + 1])
+                # takes the current word in a review and the next word in the same review and stores it in a tuple
+                eachTuple = tuple((train_set[i][j], train_set[i][j + 1]))
+
+                # checks to see if the tuple has already been seen before or not
                 if eachTuple in bigramWords:
                     bigramWords[eachTuple] += 1
                 else:
                     bigramWords[eachTuple] = 1
 
+    # calculates the probability of a tuple while applying laplace smoothing equation
+    # doing this for unknown probability, which are all the unseen tuples, and for the known probability, which are all the seen tuples
     total_types = len(bigramWords)
     for word_counter in bigramWords:
         total_words += bigramWords[word_counter]
@@ -191,7 +229,5 @@ def bigramTrainingPhase(train_set, train_labels, laplace_smoothing, type):
     unknown_prob = laplace_smoothing / (total_words + (laplace_smoothing * (total_types + 1)))
 
     return known_prob, unknown_prob
-    
-
 
 
--- MP2/mp2.py
+++ MP2/mp2.py
@@ -71,13 +71,13 @@ if __name__ == "__main__":
                         help='Use porter stemmer')
     parser.add_argument('--lowercase',dest="lowercase", type=bool, default=False,
                         help='Convert all word to lower case')
-    parser.add_argument('--laplace',dest="laplace", type=float, default = 0.15,
+    parser.add_argument('--laplace',dest="laplace", type=float, default = 0.009,
                         help='Laplace smoothing parameter')
-    parser.add_argument('--bigram_laplace',dest="bigram_laplace", type=float, default = 0.1,
+    parser.add_argument('--bigram_laplace',dest="bigram_laplace", type=float, default = 0.007,
                         help='Laplace smoothing parameter for bigrams')
-    parser.add_argument('--bigram_lambda',dest="bigram_lambda", type=float, default = 0.9,
+    parser.add_argument('--bigram_lambda',dest="bigram_lambda", type=float, default = 0.42,
                         help='Weight on bigrams vs. unigrams')
-    parser.add_argument('--pos_prior',dest="pos_prior", type=float, default = 0.75,
+    parser.add_argument('--pos_prior',dest="pos_prior", type=float, default = 0.8,
                         help='Positive prior, i.e. percentage of test examples that are positive')
 
     args = parser.parse_args()
